ea7ea413b12a761c6a41b560316cc83f4771fe42
Daniel Koch <dkoch84@gmail.com>
2022-04-28 16:39:16 -0500

    I know how to do terraform, I promise

--- state/variables.tf
+++ state/variables.tf
@@ -1,8 +1,8 @@
-location                    = { default = null }
-rg_name                     = { default = null }
-sa_name                     = { default = null }
-sa_account_tier             = { default = null }
-sa_account_replication_type = { default = null }
-c_name                      = { default = null }
-c_access_type               = { default = null }
-tags                        = { default = null }
+variable "location"                    { default = null }
+variable "rg_name"                     { default = null }
+variable "sa_name"                     { default = null }
+variable "sa_account_tier"             { default = null }
+variable "sa_account_replication_type" { default = null }
+variable "c_name"                      { default = null }
+variable "c_access_type"               { default = null }
+variable "tags"                        { default = null }
